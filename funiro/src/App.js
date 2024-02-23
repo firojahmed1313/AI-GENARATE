8367bc22d2bc08afd48505b7e9db5f392b6cddfd
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-02-23 15:19:48 +0000

    add home and summary page

---
+++ funiro/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Summary from './pages/Summary';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/summery' element={<Summary/>}></Route>
+      </Routes>
+      
+    </>
+  );
+}
+
+export default App;
