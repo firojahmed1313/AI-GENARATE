eb79cb37ae5a5fd461cca886e9be08fcfd564b2d
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-02-27 21:01:24 +0000

    input data out put oic

--- funiro/src/pages/searchImage/DisplayImage.jsx
+++ funiro/src/pages/searchImage/DisplayImage.jsx
@@ -1,27 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
+import React from 'react'
 import "./searchImage.css"
 
 
-const DisplayImage = () => {
-    const [urlData, setUrlData] = useState();
-    const url = "https://api.unsplash.com/search/photos?page=1&query=car&client_id=P5nEHiXSdgh6OZttsMSEAF51TN9BU_Om9xaon5TrPkg";
-    useEffect(() => {
-
-        const urlget = async () => {
-            try {
-                const data = await axios.get(url);
-                console.log(data.data.results[0].urls);
-                setUrlData(data.data.results);
-            } catch (error) {
-                console.warn(error);
-            }
-
-        }
-
-        urlget();
-    }, [])
-    console.log(urlData)
+const DisplayImage = ({urlData}) => {
+    
     return (
         <>
             <div className='imagecontainer'>
--- funiro/src/pages/searchImage/SearchImage.jsx
+++ funiro/src/pages/searchImage/SearchImage.jsx
@@ -3,15 +3,42 @@ import "./searchImage.css"
 import axios from "axios"
 import DisplayImage from './DisplayImage';
 const SearchImage = () => {
+  const [searchitem, setsearchitem] = useState("");
   const [urlData, setUrlData] = useState();
-  
+  const url = `https://api.unsplash.com/search/photos?page=1&query=${searchitem}&client_id=P5nEHiXSdgh6OZttsMSEAF51TN9BU_Om9xaon5TrPkg`;
+  console.log(url);
+  useEffect(() => {
+
+    const urlget = async () => {
+      try {
+        const data = await axios.get(url);
+        console.log(data.data.results[0].urls);
+        setUrlData(data.data.results);
+      } catch (error) {
+        console.warn(error);
+      }
+
+    }
+
+    urlget();
+  }, [searchitem])
+  console.log(urlData)
+  console.log(searchitem);
   return (
     <>
       <div className='container'>
+        <h2>Enter Keyword For Search : </h2>
         <div className='searchItemdiv'>
-            <input placeholder='Enter Keyword'  />
+          <input
+            placeholder='Enter Keyword'
+            className='inputSearch'
+            name='searchitem'
+            type='text'
+            value={searchitem}
+            onChange={(e) => setsearchitem(e.target.value)}
+          />
         </div>
-        <DisplayImage/>
+        <DisplayImage urlData={urlData} />
       </div>
     </>
   )
